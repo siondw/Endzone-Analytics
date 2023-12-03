76da603c8d524447691e0b2c2a5fa1a72a97fbab
[amaeypandit] <[amaey.pandit1@gmail.com]>
2023-12-03 15:05:58 -0500

    3pm

--- flask-app/src/teams/teams.py
+++ flask-app/src/teams/teams.py
@@ -1,32 +1,37 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
+from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
 
 
-products = Blueprint('teams', __name__)
+customers = Blueprint('teams', __name__)
 
-# Get all the products from the database
+# Get all customers from the DB
 @teams.route('/teams', methods=['GET'])
 def get_teams():
-    # get a cursor object from the database
     cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of products
     cursor.execute('SELECT team_abbr, name, division, conference,salary_cap,third_conv_rate,redzone_eff,avg_ticket_price,wins,losses,win_pct FROM NFLTeams')
 
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
+    row_headers = [x[0] for x in cursor.description]
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
-    return jsonify(json_data)
+# Get customer detail for customer with particular userID
+@customers.route('/customers/<userID>', methods=['GET'])
+def get_customer(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from customers where id = {0}'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
