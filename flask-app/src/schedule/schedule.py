95923869e574989170ab8f52da78a468423266ed
[amaeypandit] <[amaey.pandit1@gmail.com]>
2023-12-04 18:52:00 -0500

    teams

--- flask-app/src/schedule/schedule.py
+++ flask-app/src/schedule/schedule.py
@@ -41,3 +41,24 @@ def get_team_schedule(team_abbr):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# Get all teams from the DB
+@schedule.route('/schedule/teams', methods=['GET'])
+def get_teams():
+
+    query = f"SELECT team_abbr FROM NFLTeams"
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
