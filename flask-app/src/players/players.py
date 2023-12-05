77d54cca748d48991b431f1203c5d51ef63500ec
alextang <alxhtang@gmail.com>
2023-12-04 19:49:55 -0500

    players

--- flask-app/src/players/players.py
+++ flask-app/src/players/players.py
@@ -23,10 +23,46 @@ def get_players(name):
     the_response.mimetype = 'application/json'
     return the_response
 
-# show leader board
-@players.route('/players', methods=['GET'])
-def get_leaderboard():
-    query = f"SELECT player_name, pass_yards_total FROM Players ORDER BY pass_yards_total DESC LIMIT 5'"
+# passing leader board
+@players.route('/players/passing_leaders', methods=['GET'])
+def get_passing_leaderboard():
+    query = f"SELECT player_name, pass_yards_total FROM Players ORDER BY pass_yards_total DESC LIMIT 10'"
+    current_app.logger.info(query)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# rushing leader board
+@players.route('/players/rushing_leaders', methods=['GET'])
+def get_passing_leaderboard():
+    query = f"SELECT player_name, rush_yards_total FROM Players ORDER BY rush_yards_total DESC LIMIT 10'"
+    current_app.logger.info(query)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# receiving leader board
+@players.route('/players/receiving_leaders', methods=['GET'])
+def get_passing_leaderboard():
+    query = f"SELECT player_name, rec_yards_total FROM Players ORDER BY rec_yards_total DESC LIMIT 10'"
     current_app.logger.info(query)
     cursor = db.get_db().cursor()
     cursor.execute(query)
