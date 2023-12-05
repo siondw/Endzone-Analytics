e993f06d808212a77d65d46d286e592c5663a8f1
[amaeypandit] <[amaey.pandit1@gmail.com]>
2023-12-05 12:41:38 -0500

    init changes

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -33,8 +33,6 @@ def create_app():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
     # Import the various Beluprint Objects
-    # from src.customers.customers import customers
-    # from src.products.products  import products
     from src.teams.teams  import teams
     from src.schedule.schedule import schedule
     from src.players.players import players
@@ -42,8 +40,6 @@ def create_app():
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
-    # app.register_blueprint(customers,   url_prefix='/c')
-    # app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(teams,    url_prefix='/t')
     app.register_blueprint(schedule,    url_prefix='/s')
     app.register_blueprint(players,    url_prefix='/p')
