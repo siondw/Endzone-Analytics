7652346202e49e3707f1f0cb399aa7e38ad42c10
[amaeypandit] <[amaey.pandit1@gmail.com]>
2023-12-05 16:29:21 -0500

    NFC AFC

--- flask-app/src/teams/teams.py
+++ flask-app/src/teams/teams.py
@@ -5,11 +5,11 @@ from src import db
 
 teams = Blueprint('teams', __name__)
 
-# Get standings
-@teams.route('/standings', methods=['GET'])
-def get_standings():
+# Get standings for NFC
+@teams.route('/standings/NFC', methods=['GET'])
+def get_standings_NFC():
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT team_name, SUM(wins), SUM(losses), `division` FROM NFLTeams GROUP BY `division`, `team_name` ORDER BY `division`, SUM(wins) DESC;')
+    cursor.execute("SELECT team_name, SUM(wins), SUM(losses), `division` FROM NFLTeams WHERE `conference` = 'NFC' GROUP BY `division`, `team_name` ORDER BY `division`, SUM(wins) DESC;")
 
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -21,6 +21,21 @@ def get_standings():
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get standings for NFC
+@teams.route('/standings/AFC', methods=['GET'])
+def get_standings_AFC():
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT team_name, SUM(wins), SUM(losses), `division` FROM NFLTeams WHERE `conference` = 'AFC' GROUP BY `division`, `team_name` ORDER BY `division`, SUM(wins) DESC;")
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # Get all the stats from the DB for a specific team
 @teams.route('/teams/<team_abbr>', methods=['GET'])
