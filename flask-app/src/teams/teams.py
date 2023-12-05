e7ba97fd9befa4cd8446444006e36f3df31209c8
[amaeypandit] <[amaey.pandit1@gmail.com]>
2023-12-05 12:33:56 -0500

    team

--- flask-app/src/teams/teams.py
+++ flask-app/src/teams/teams.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -10,6 +10,33 @@ teams = Blueprint('teams', __name__)
 def get_standings():
     cursor = db.get_db().cursor()
     cursor.execute('SELECT team_name, SUM(wins), SUM(losses), `division` FROM NFLTeams GROUP BY `division`, `team_name` ORDER BY `division`, SUM(wins) DESC;')
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+# Get all the stats from the DB for a specific team
+@teams.route('/teams/<team_abbr>', methods=['GET'])
+def get_team_stats(team_abbr):
+
+    # query = f"SELECT * FROM NFLTeams WHERE team_abbr = '{team_abbr}'"
+    # current_app.logger.info(query)
+
+    # cursor = db.get_db().cursor()
+    # cursor.execute(query)
+
+    query = f"SELECT * FROM NFLTeams NATURAL JOIN Team_Game NATURAL JOIN Game WHERE team_abbr =  '{team_abbr}'"
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
 
     row_headers = [x[0] for x in cursor.description]
     json_data = []
