0dad9dad037bed56ee04d70a5a0488338fc3f979
alextang <alxhtang@gmail.com>
2023-12-05 23:22:42 -0500

    injury post

--- flask-app/src/players/players.py
+++ flask-app/src/players/players.py
@@ -240,3 +240,29 @@ def get_game_stats(player_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# add a player injury
+@players.route('/players/injuries/<player_id>', methods=['POST'])
+def add_injury(player_id):
+   # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    injury = the_data['injury']
+    duration = the_data['duration']
+ 
+
+   # Constructing the query with manual quotes for string fields
+    query =  'INSERT INTO Player_Injuries (player_id, injury, duration) VALUES ('
+    query += "'" + player_id + "', '"
+    query += injury + "', '"
+    query += duration + ')'
+    current_app.logger.info(query)
+
+    # Executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
